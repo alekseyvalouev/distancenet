0f436fcada20ebb434e045e451029d7953851a07
Aleksey Valouev <alekseyvalouev@gmail.com>
2026-03-03 13:42:39 -0800

    bigger moel

--- models.py
+++ models.py
@@ -36,14 +36,23 @@ class PairwiseDistanceNet(nn.Module):
     def __init__(self, n_classes=5, backbone="dinov2_vits14_reg"):
         super().__init__()
         self.backbone = DINOv2_Backbone(backbone)
+        self.n_classes = n_classes
         if (n_classes == None):
-            self.fc = nn.Linear(2 * 384, 1)
+            self.x1 = nn.Linear(2*384, 512)
+            self.a1 = nn.ReLU()
+            self.x2 = nn.Linear(512, 128)
+            self.a2 = nn.ReLU()
+            self.fc = nn.Linear(128, 1)
         else:
             self.fc = nn.Linear(2 * 384, n_classes)
     
     def forward(self, x1, x2):
-        return self.fc(torch.cat([self.backbone(x1), self.backbone(x2)], dim=1))
+        assert self.n_classes is None
+        feats = torch.cat([self.backbone(x1), self.backbone(x2)], dim=1)
+        feats = self.a1(self.x1(feats))
+        feats = self.a2(self.x2(feats))
+        return self.fc(feats)
 
 if __name__ == "__main__":
-    model = PairwiseDistanceNet()
+    model = PairwiseDistanceNet(n_classes=None)
     print(model)
